173f8dd6c2575516e5df69a3caed9619327aeca6
Waldox2004 <walid.elmadi04@gmail.com>
2025-10-13 18:58:28 +0200

    Test fail fizzbuz

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+class fizzbuzz:
+   def __init__(self):
+       pass
+
+
+       for i in range(1, 101):
+           if i % 15 == 0:
+               res += "FrisBee"
+           elif i % 3 == 0:
+               res += "Fizz"
+           elif i % 5 == 0:
+               res += "Buzz"
+           else:
+               res += str(i)
+       return res
---
+++ test_fizzbuzz.py
@@ -0,0 +1,13 @@
+import unittest
+from fizzbuzz import affiche
+
+
+class TestFizzBuzz(unittest.TestCase):
+   def setUp(self):
+       self.instance=fizzbuzz()
+   def test_affiche_sans_param(self):
+       self.assertEqual(self.instance.affiche(), "12fizz4buzzfizz")
+
+
+if __name__ == '__main__':
+   unittest.main()
