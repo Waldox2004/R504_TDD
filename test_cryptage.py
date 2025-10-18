02dc6efd43248ab0f1658a0cd7b202d6cb2b1fa8
Waldox2004 <walid.elmadi04@gmail.com>
2025-10-18 08:49:15 +0200

    Test B fail crypt

--- test_cryptage.py
+++ test_cryptage.py
@@ -4,5 +4,8 @@ from cryptage import crypt
 def test_crypt_simple():
     assert crypt("abc") == "bcd"
 
+def test_crypt_avec_pas():
+    assert crypt("abc", 2) == "cde2"
+
 if __name__ == '__main__':
     unittest.main()
