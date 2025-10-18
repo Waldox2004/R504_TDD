dcbad36107339a31b7779a9048d73e036069537c
Waldox2004 <walid.elmadi04@gmail.com>
2025-10-18 09:23:03 +0200

    Test C sucess crypt

--- cryptage.py
+++ cryptage.py
@@ -3,3 +3,11 @@ def crypt(message, pas=1):
     for c in message:
         res += chr(ord(c) + pas)
     return res + str(pas)
+
+def decrypt(message):
+    pas = int(message[-1])
+    corps = message[:-1]
+    res = ""
+    for c in corps:
+        res += chr(ord(c) - pas)
+    return res
